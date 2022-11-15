9807639f62bcd642b1e8ac682757b38de60f0179
GabrielCarvalhoM <c.gabrielmaciel@icloud.com>
2022-11-14 21:13:06 -0300

    Initial Commit

---
+++ Desafio-Ws-Work/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Desafio-Ws-Work
+//
+//  Created by Gabriel Carvalho on 14/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
